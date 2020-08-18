66547e7ca56e07e5b787dfcd483cc914fe59e034
Jens Hilligsøe <jens@hilli.dk>
2020-08-18 13:34:15 +0200

    Spit out data to statsd

--- finance-statsd.go
+++ finance-statsd.go
@@ -2,26 +2,64 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/DataDog/datadog-go/statsd"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/piquette/finance-go/equity"
 )
 
 // Uses https://piquette.io/projects/finance-go/ https://github.com/piquette/finance-go
 
 func main() {
-	symbols := []string{"AAPL", "TSLA", "MSFT"}
-	iter := equity.List(symbols)
-
-	// Iterate over results. Will exit upon any error.
-	for iter.Next() {
-		q := iter.Equity()
-		fmt.Println(q.Symbol, "(", q.ShortName, "): Bid: ", q.Bid, " Ask: ", q.Ask, "Price:", q.RegularMarketPrice)
-		fmt.Printf("High: %.2f Low: %.2f Close: %.2f Post: %.2f\n", q.RegularMarketDayHigh, q.RegularMarketDayLow, q.RegularMarketPreviousClose, q.RegularMarketPrice+q.PreMarketChange)
+	// Setup
+	statsdEndpoint := os.Getenv("STATSD_ENDPOINT")
+	if statsdEndpoint == "" {
+		statsdEndpoint = "127.0.0.1:8125"
+	}
+	statsd, err := statsd.New(statsdEndpoint)
+	statsd.Namespace = "stocks."
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Catch an error, if there was one.
-	if iter.Err() != nil {
-		// Uh-oh!
-		panic(iter.Err())
+	// Setup collection interval
+	ci, _ := strconv.ParseUint(os.Getenv("COLLECTION_INTERVAL"), 10, 32)
+	if ci == 0 {
+		ci = 10 // ci probably unset - But if set to 0 then don't DoS the bridge
+	}
+	collectionInterval := time.Duration(ci) * time.Second
+
+	symbols := strings.Split(os.Getenv("SYMBOLS"), ",")
+
+	for {
+		iter := equity.List(symbols)
+
+		// Iterate over results. Will exit upon any error.
+		for iter.Next() {
+			q := iter.Equity()
+			if os.Getenv("DEBUG") != "" {
+				fmt.Println(q.Symbol, "(", q.ShortName, "): Bid: ", q.Bid, " Ask: ", q.Ask, "Price:", q.RegularMarketPrice)
+				fmt.Printf("     High: %.2f Low: %.2f Close: %.2f Post: %.2f\n", q.RegularMarketDayHigh, q.RegularMarketDayLow, q.RegularMarketPreviousClose, q.RegularMarketPrice+q.PreMarketChange)
+			}
+			statsd.Gauge("bid", q.Bid, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("ask", q.Ask, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("price", q.RegularMarketPrice, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("high", q.RegularMarketDayHigh, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("low", q.RegularMarketDayLow, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("prev_close", q.RegularMarketPreviousClose, []string{"symbol:" + q.Symbol}, 1)
+			statsd.Gauge("post", q.RegularMarketPrice+q.PreMarketChange, []string{"symbol:" + q.Symbol}, 1)
+		}
+
+		// Catch an error, if there was one.
+		if iter.Err() != nil {
+			// Uh-oh!
+			panic(iter.Err())
+		}
+		time.Sleep(collectionInterval)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/hilli/finance-statsd
 go 1.15
 
 require (
+	github.com/DataDog/datadog-go v3.7.2+incompatible // indirect
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/piquette/finance-go v1.0.0 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
 )
