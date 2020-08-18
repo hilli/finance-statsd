37ad22769a50fafd171663b028a0624370e2e989
Jens Hilligsøe <jens@hilli.dk>
2020-08-18 12:40:34 +0200

    Found the number I want for this

--- finance-statsd.go
+++ finance-statsd.go
@@ -3,17 +3,24 @@ package main
 import (
 	"fmt"
 
-	"github.com/piquette/finance-go/quote"
+	"github.com/piquette/finance-go/equity"
 )
+
+// Uses https://piquette.io/projects/finance-go/ https://github.com/piquette/finance-go
 
 func main() {
 	symbols := []string{"AAPL", "TSLA", "MSFT"}
-	iter := quote.List(symbols)
+	// iter := quote.List(symbols)
+	iter := equity.List(symbols)
 
 	// Iterate over results. Will exit upon any error.
 	for iter.Next() {
-		q := iter.Quote()
-		fmt.Println(q.Symbol, ": ", q.Bid)
+		// q := iter.Quote()
+		q := iter.Equity()
+		fmt.Println(q.Symbol, "(", q.ShortName, "): Bid: ", q.Bid, " Ask: ", q.Ask, "Price:", q.RegularMarketPrice)
+		fmt.Println("High:", q.RegularMarketDayHigh, "Low:", q.RegularMarketDayLow, "Close:", q.RegularMarketPreviousClose)
+		fmt.Println()
+
 	}
 
 	// Catch an error, if there was one.
